f7d37364475acdd2a33cf74f3b08b7b08fc0371e
Jatinsharma43 <jatin0237@gmail.com>
2025-02-28 14:58:24 +0530

    fixed bug

--- src/index.js
+++ src/index.js
@@ -2,17 +2,7 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="red" maxRating={5} onSetRating={setMovieRating} />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
